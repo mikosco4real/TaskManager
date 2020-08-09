52767ce4d879756328b393faf303b6774e5daeb4
Michael Okolo <okolomikel2009@gmail.com>
2020-08-09 23:40:08 +1000

    Started working in my branch

--- header/model.h
+++ header/model.h
@@ -3,15 +3,20 @@
 #include <fstream>
 
 class BaseModel
+/*
+    This is a base class that defines how data is saved to disk.
+*/
 {
    public:
    std::string filename{};
    std::fstream file;
 
    BaseModel(std::string filename) : filename(filename){
+       // Open file for read and write operations
        file.open(filename, std::fstream::app | std::fstream::out | std::fstream::in);
    }
    ~BaseModel(){
+       // Close file when this object is destroyed
        file.close();
    }
 };
