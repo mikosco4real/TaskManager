91025775c09126171505011e02a4a8cca821d147
diegoarojas <69175656+diegoarojas@users.noreply.github.com>
2020-08-17 17:20:46 +1000

    Update tasksGroupMap.cpp
    TaskGroupMap

--- src/tasksGroupMap.cpp
+++ src/tasksGroupMap.cpp
@@ -7,3 +7,26 @@ TasksGroupMap::TasksGroupMap()
 TasksGroupMap::~TasksGroupMap()
 {
 }
+
+void TasksGroupMap::createGroupMap()
+{
+
+}
+
+void TasksGroupMap::getUsers()
+{
+
+}
+
+void TasksGroupMap::getGroups()
+{
+
+}
+
+void TasksGroupMap::disableMap()
+{
+        
+}
+
+
+
